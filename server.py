0c71f6215e4606d2ec51e10343a9b4b5ec1d6e56
denyanenko <108097508+denyanenko@users.noreply.github.com>
2024-03-17 23:43:34 +0200

    dbdf

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
-from EmotionDetection.emotion_detection import emotion_detector
+from flask import Flask, request
+from EmotionDetection import emotion_detector
 
 app = Flask(__name__)
 
@@ -7,22 +7,14 @@ app = Flask(__name__)
 def emotionDetector():
     data = request.json
     text_to_analyze = data['text']
-    
+    print(text_to_analyze)
     result = emotion_detector(text_to_analyze)
     
-    response = {
-        "anger": result['anger'], 
-        "disgust": result['disgust'], 
-        "fear": result['fear'], 
-        "joy": result['joy'], 
-        "sadness": result['sadness'], 
-        "dominant_emotion": result['dominant_emotion']
-    }
-    
+ 
     output_text = "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(
         result['anger'], result['disgust'], result['fear'], result['joy'], result['sadness'], result['dominant_emotion'])
     
-    return jsonify(response), output_text
+    return output_text
 
 if __name__ == '__main__':
     app.run(debug=True)
